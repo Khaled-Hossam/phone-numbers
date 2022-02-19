4cae91b1e2e7098c391950ad2e9ade8be87a3128
Khaled Hossam <khaledhossam10@outlook.com>
2022-02-19 18:08:47 +0200

    use api resource for phone number response

--- app/Http/Controllers/CustomerPhoneController.php
+++ app/Http/Controllers/CustomerPhoneController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\CustomerPhoneFilterRequest;
+use App\Http\Requests\PhoneNumberFilterRequest;
+use App\Http\Resources\PhoneNumberResource;
 use App\Services\CategorizePhoneNumbersService;
-use App\Services\CustomerFilterService;
+use App\Services\CustomerPhoneFilterService;
 use Illuminate\Http\Request;
 
 class CustomerPhoneController extends Controller
@@ -16,21 +17,21 @@ class CustomerPhoneController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function __invoke(
-        CustomerPhoneFilterRequest $request,
+        PhoneNumberFilterRequest $request,
         CategorizePhoneNumbersService $categorizePhoneNumbersService,
-        CustomerFilterService $customerFilterService,
+        CustomerPhoneFilterService $customerPhoneFilterService,
     )
     {
         $phonesCategorizedByCountry = $categorizePhoneNumbersService->execute();
 
         if($request->has('country_code')){
-            $phonesCategorizedByCountry = $customerFilterService->filterByCountryCode($phonesCategorizedByCountry, $request->input('country_code'));
+            $phonesCategorizedByCountry = $customerPhoneFilterService->filterByCountryCode($phonesCategorizedByCountry, $request->input('country_code'));
         }
 
         if($request->has('is_valid_phones')){
-            $phonesCategorizedByCountry = $customerFilterService->filterByPhoneValidity($phonesCategorizedByCountry, $request->input('is_valid_phones'));
+            $phonesCategorizedByCountry = $customerPhoneFilterService->filterByPhoneValidity($phonesCategorizedByCountry, $request->input('is_valid_phones'));
         }
 
-        return $phonesCategorizedByCountry;
+        return PhoneNumberResource::collection( $phonesCategorizedByCountry);
     }
 }
---
+++ app/Http/Resources/PhoneNumberResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PhoneNumberResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'country' => $this->country,
+            'country_code' => '+' . $this->country_code,
+            'is_valid_phone' => $this->is_valid_phone,
+            'phone_without_country_code' => $this->phone_without_country_code,
+        ];
+
+        return parent::toArray($request);
+    }
+}
