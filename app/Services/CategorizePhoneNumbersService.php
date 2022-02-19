1402e492cbf430728cf437fd3f7f1f049bba486b
Khaled Hossam <khaledhossam10@outlook.com>
2022-02-19 18:10:11 +0200

    add phone without code and remove unused code from categorize phones service

--- app/Services/CategorizePhoneNumbersService.php
+++ app/Services/CategorizePhoneNumbersService.php
@@ -13,17 +13,11 @@ class CategorizePhoneNumbersService {
             ->map(function(Customer $customer){
                 preg_match('/\((\d+?)\)/', $customer->phone, $match);
                 $customer->country_code = $match[1];
-                $customer->valid_phone = $this->isValidPhoneNumber($customer->country_code, $customer->phone);
+                $customer->phone_without_country_code = str_replace($match[0], '', $customer->phone);
+                $customer->is_valid_phone = $this->isValidPhoneNumber($customer->country_code, $customer->phone);
                 $customer->country = CountryEnum::DATA[$customer->country_code]['country_name'];
                 return $customer;
             })
-            // ->groupBy('country_code')
-            // ->when($countryCode, function(Collection $collection) use($countryCode){
-            //     $collection->filter(fn($customer)=> $customer->country_code == $countryCode);
-            // })
-            // ->when($isValidNumber, function(Collection $collection) use($isValidNumber){
-            //     $collection->filter(fn($customer)=> $customer->valid_phone == $isValidNumber);
-            // })
             ;
 
         return $phonesCategorizedByCountry;
