b082dcabc883197cf01538b604014074c1450067
Khaled Hossam <khaledhossam10@outlook.com>
2022-02-19 17:55:19 +0200

    rename phone filter service

--- app/Services/CustomerPhoneFilterService.php
+++ app/Services/CustomerPhoneFilterService.php
@@ -4,14 +4,14 @@ namespace App\Services;
 
 use Illuminate\Support\Collection;
 
-class CustomerFilterService {
+class CustomerPhoneFilterService {
 
     public function filterByCountryCode(Collection $collection, $countryCode){
         return $collection->filter(fn($customer)=> $customer->country_code == $countryCode);
     }
 
     public function filterByPhoneValidity(Collection $collection, $phoneValidity){
-        return $collection->filter(fn($customer)=> (bool)$customer->valid_phone === (bool)$phoneValidity);
+        return $collection->filter(fn($customer)=> (bool)$customer->is_valid_phone === (bool)$phoneValidity);
     }
 
 }
